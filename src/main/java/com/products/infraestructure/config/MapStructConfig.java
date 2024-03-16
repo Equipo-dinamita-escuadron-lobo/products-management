41b8370fe51367083bd42cb9fc1d5c8f8d2ee649
Jamesss73 <jamesmunozs@unicauca.edu.co>
2024-03-16 15:00:07 -0500

    agregue la aceptacion de cors para las solicitudes http

--- src/main/java/com/products/infraestructure/config/MapStructConfig.java
+++ src/main/java/com/products/infraestructure/config/MapStructConfig.java
@@ -1,15 +1,24 @@
 package com.products.infraestructure.config;
 
+import com.products.infraestructure.input.Rest.Mapper.IProductRestMapper;
+import com.products.infraestructure.ouput.jpaAdapter.Mapper.IProductMapper;
 import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.products.infraestructure.input.Rest.Mapper.IProductRestMapper;
-import com.products.infraestructure.ouput.jpaAdapter.Mapper.IProductMapper;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MapStructConfig {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST");
+            }
+        };
+    }
     @Bean
     IProductMapper MapGetProductMapper() {
         return Mappers.getMapper(IProductMapper.class);

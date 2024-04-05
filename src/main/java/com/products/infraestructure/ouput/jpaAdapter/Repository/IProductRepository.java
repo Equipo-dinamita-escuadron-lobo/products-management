2118748c8fdd5580d532633647c1c619d1bcecf7
JhossefCons <jhniconstain@unicauca.edu.co>
2024-04-04 23:44:24 -0500

    sin error

--- src/main/java/com/products/infraestructure/ouput/jpaAdapter/Repository/IProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.products.infraestructure.ouput.jpaAdapter.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.products.infraestructure.ouput.jpaAdapter.Entity.ProductEntity;
-
-public interface IProductRepository extends JpaRepository<ProductEntity, String>{
-    
-}

4ab7742e673199535ad0362b4d0cdb3e323133be
Janier Gomez <jygomezg211@gmail.com>
2024-12-09 20:00:30 -0500

    Documentación Swagger 2

--- src/main/java/com/products_management/infraestructure/input/rest/ProductTypeRestController.java
+++ src/main/java/com/products_management/infraestructure/input/rest/ProductTypeRestController.java
@@ -4,6 +4,13 @@ import com.products_management.domain.model.ProductType;
 import com.products_management.infraestructure.input.rest.mapper.interfaces.IProductTypeRestMapper;
 import com.products_management.infraestructure.input.rest.model.request.ProductTypeRequest;
 import com.products_management.infraestructure.input.rest.model.response.ProductTypeResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import com.products_management.application.ports.input.IProductTypeServicePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,6 +33,11 @@ public class ProductTypeRestController {
     private final IProductTypeServicePort productTypeService;
     private final IProductTypeRestMapper productTypeMapper;
 
+    @Operation(summary = "Crear un nuevo tipo de producto", description = "Este endpoint permite crear un nuevo tipo de producto utilizando los datos proporcionados en el cuerpo de la solicitud.", responses = {
+            @ApiResponse(responseCode = "201", description = "Tipo de producto creado exitosamente", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida, los datos proporcionados no son correctos", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Error interno al crear el tipo de producto", content = @Content(mediaType = "application/json"))
+    })
     @PostMapping
     public ResponseEntity<ProductTypeResponse> createProductType(@RequestBody ProductTypeRequest productTypeRequest) {
         ProductType productType = productTypeMapper.toProductType(productTypeRequest);
@@ -34,6 +46,10 @@ public class ProductTypeRestController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Obtener todos los tipos de productos", description = "Este endpoint permite obtener una lista de todos los tipos de productos disponibles en el sistema.", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista de tipos de productos obtenida con éxito", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Error interno al obtener los tipos de productos", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping
     public ResponseEntity<List<ProductTypeResponse>> getAllProductTypes() {
         List<ProductType> productTypes = productTypeService.listAllProductTypes();
@@ -43,6 +59,11 @@ public class ProductTypeRestController {
         return ResponseEntity.ok(responses);
     }
 
+    @Operation(summary = "Obtener tipos de productos por ID de empresa", description = "Este endpoint permite obtener una lista de tipos de productos asociados a una empresa específica.", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista de tipos de productos obtenida con éxito para la empresa especificada", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Empresa no encontrada", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Error interno al obtener los tipos de productos", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/enterprise/{enterpriseId}")
     public ResponseEntity<List<ProductTypeResponse>> getProductTypesByEnterpriseId(@PathVariable String enterpriseId) {
         List<ProductType> productTypes = productTypeService.getProductTypesByEnterpriseId(enterpriseId);
@@ -52,6 +73,10 @@ public class ProductTypeRestController {
         return ResponseEntity.ok(responses);
     }
 
+    @Operation(summary = "Obtener un tipo de producto por su identificador", description = "Recupera un tipo de producto específico utilizando el identificador proporcionado.", responses = {
+            @ApiResponse(responseCode = "200", description = "Tipo de producto encontrado", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Tipo de producto no encontrado", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ProductTypeResponse> getProductTypeById(@PathVariable Long id) {
         Optional<ProductType> productTypeOptional = productTypeService.findById(id);
@@ -62,6 +87,11 @@ public class ProductTypeRestController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Actualizar un tipo de producto", description = "Permite actualizar un tipo de producto existente utilizando su identificador y los datos proporcionados.", responses = {
+            @ApiResponse(responseCode = "200", description = "Tipo de producto actualizado", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Tipo de producto no encontrado", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta o datos inválidos", content = @Content(mediaType = "application/json"))
+    })
     @PutMapping("/{id}")
     public ResponseEntity<ProductTypeResponse> updateProductType(
             @PathVariable Long id,
@@ -72,6 +102,10 @@ public class ProductTypeRestController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Eliminar un tipo de producto", description = "Permite eliminar un tipo de producto utilizando su identificador.", responses = {
+            @ApiResponse(responseCode = "204", description = "Tipo de producto eliminado exitosamente", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Tipo de producto no encontrado", content = @Content(mediaType = "application/json"))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProductType(@PathVariable Long id) {
         productTypeService.deleteProductType(id);

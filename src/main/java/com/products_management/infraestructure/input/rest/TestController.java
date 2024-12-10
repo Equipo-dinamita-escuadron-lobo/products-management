e49947c1be820d541577d14c4894bf14afbf8aab
Janier Gomez <jygomezg211@gmail.com>
2024-12-09 19:49:36 -0500

    Documentación Swagger

--- src/main/java/com/products_management/infraestructure/input/rest/TestController.java
+++ src/main/java/com/products_management/infraestructure/input/rest/TestController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 /**
  * Controlador de prueba para verificar el funcionamiento del servicio.
  */
@@ -13,10 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/categories/test")
 public class TestController {
 
-    /**
-     * Método para verificar la disponibilidad del servicio.
-     * @return Cadena "pong" como respuesta exitosa.
-     */
+    @Operation(summary = "Verificar la disponibilidad del servicio", description = "Este endpoint verifica que el servicio esté activo y responde con un mensaje 'pong'.", responses = {
+            @ApiResponse(responseCode = "200", description = "Servicio disponible", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/ping")
     public String ping() {
         return "pong";
--- src/main/java/com/products_management/infraestructure/input/rest/UnitMeasureRestController.java
+++ src/main/java/com/products_management/infraestructure/input/rest/UnitMeasureRestController.java
@@ -4,6 +4,10 @@ import com.products_management.application.ports.input.IUnitOfMeasureServicePort
 import com.products_management.infraestructure.input.rest.mapper.impl.UnitOfMeasureRestMapperImpl;
 import com.products_management.infraestructure.input.rest.model.request.UnitOfMeasureCreateRequest;
 import com.products_management.infraestructure.input.rest.model.response.UnitOfMeasureResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,81 +28,82 @@ public class UnitMeasureRestController {
     private final IUnitOfMeasureServicePort unitOfMeasureServicePort;
     private final UnitOfMeasureRestMapperImpl unitOfMeasureRestMapper;
 
-    /**
-     * Método para obtener todas las unidades de medida de una empresa.
-     * @param enterpriseId Identificador de la empresa.
-     * @return Lista de respuestas de unidades de medida.
-     */
+    @Operation(summary = "Obtener todas las unidades de medida de una empresa", description = "Este endpoint devuelve una lista de todas las unidades de medida asociadas a una empresa específica.", responses = {
+            @ApiResponse(responseCode = "200", description = "Unidades de medida obtenidas con éxito", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Empresa no encontrada", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/findAll/{enterpriseId}")
     public List<UnitOfMeasureResponse> findAll(@PathVariable String enterpriseId) {
         return unitOfMeasureRestMapper.toUnitOfMeasureResponseList(unitOfMeasureServicePort.findAll(enterpriseId));
     }
 
-    /**
-     * Método para obtener todas las unidades de medida activadas de una empresa.
-     * @param enterpriseId Identificador de la empresa.
-     * @return Lista de respuestas de unidades de medida activadas.
-     */
+    @Operation(summary = "Obtener todas las unidades de medida activadas de una empresa", description = "Este endpoint devuelve una lista de todas las unidades de medida activadas asociadas a una empresa específica.", responses = {
+            @ApiResponse(responseCode = "200", description = "Unidades de medida activadas obtenidas con éxito", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Empresa no encontrada", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/findActivate/{enterpriseId}")
     public List<UnitOfMeasureResponse> findActivate(@PathVariable String enterpriseId) {
-        return unitOfMeasureRestMapper.toUnitOfMeasureResponseList(unitOfMeasureServicePort.findActivated(enterpriseId));
+        return unitOfMeasureRestMapper
+                .toUnitOfMeasureResponseList(unitOfMeasureServicePort.findActivated(enterpriseId));
     }
 
-    /**
-     * Método para encontrar una unidad de medida por su identificador.
-     * @param id Identificador de la unidad de medida.
-     * @return Respuesta de la unidad de medida encontrada.
-     */
+    @Operation(summary = "Obtener una unidad de medida por su identificador", description = "Este endpoint permite obtener una unidad de medida específica mediante su identificador único.", responses = {
+            @ApiResponse(responseCode = "200", description = "Unidad de medida encontrada", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Unidad de medida no encontrada", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/findById/{id}")
     public UnitOfMeasureResponse findById(@PathVariable Long id) {
         return unitOfMeasureRestMapper.toUnitOfMeasureResponse(unitOfMeasureServicePort.findById(id));
     }
 
-    /**
-     * Método para crear una nueva unidad de medida.
-     * @param unitOfMeasureCreateRequest Datos de la unidad de medida a crear.
-     * @return Respuesta de la unidad de medida creada con estado HTTP 201.
-     */
+    @Operation(summary = "Crear una nueva unidad de medida", description = "Este endpoint permite crear una nueva unidad de medida utilizando los datos proporcionados en el cuerpo de la solicitud.", responses = {
+            @ApiResponse(responseCode = "201", description = "Unidad de medida creada con éxito", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/create")
-    public ResponseEntity<UnitOfMeasureResponse> create(@Valid @RequestBody UnitOfMeasureCreateRequest unitOfMeasureCreateRequest) {
+    public ResponseEntity<UnitOfMeasureResponse> create(
+            @Valid @RequestBody UnitOfMeasureCreateRequest unitOfMeasureCreateRequest) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(unitOfMeasureRestMapper.toUnitOfMeasureResponse(
-                        unitOfMeasureServicePort.create(unitOfMeasureRestMapper.toUnitOfMeasure(unitOfMeasureCreateRequest))));
+                        unitOfMeasureServicePort
+                                .create(unitOfMeasureRestMapper.toUnitOfMeasure(unitOfMeasureCreateRequest))));
     }
 
-    /**
-     * Método para actualizar una unidad de medida existente.
-     * @param id Identificador de la unidad de medida a actualizar.
-     * @param unitOfMeasureCreateRequest Datos actualizados de la unidad de medida.
-     * @return Respuesta de la unidad de medida actualizada.
-     */
+    @Operation(summary = "Actualizar una unidad de medida existente", description = "Este endpoint permite actualizar los datos de una unidad de medida existente utilizando el identificador y la información actualizada proporcionada en el cuerpo de la solicitud.", responses = {
+            @ApiResponse(responseCode = "200", description = "Unidad de medida actualizada con éxito", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Unidad de medida no encontrada", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content(mediaType = "application/json"))
+    })
     @PutMapping("/update/{id}")
-    public UnitOfMeasureResponse update(@PathVariable Long id, @Valid @RequestBody UnitOfMeasureCreateRequest unitOfMeasureCreateRequest) {
+    public UnitOfMeasureResponse update(@PathVariable Long id,
+            @Valid @RequestBody UnitOfMeasureCreateRequest unitOfMeasureCreateRequest) {
         return unitOfMeasureRestMapper.toUnitOfMeasureResponse(
-                unitOfMeasureServicePort.update(id, unitOfMeasureRestMapper.toUnitOfMeasure(unitOfMeasureCreateRequest)));
+                unitOfMeasureServicePort.update(id,
+                        unitOfMeasureRestMapper.toUnitOfMeasure(unitOfMeasureCreateRequest)));
     }
 
-    /**
-     * Método para cambiar el estado de activación de una unidad de medida.
-     * @param id Identificador de la unidad de medida a cambiar de estado.
-     */
+    @Operation(summary = "Cambiar el estado de activación de una unidad de medida", description = "Este endpoint permite cambiar el estado de activación de una unidad de medida utilizando su identificador.", responses = {
+            @ApiResponse(responseCode = "200", description = "Estado de la unidad de medida cambiado con éxito", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Unidad de medida no encontrada", content = @Content(mediaType = "application/json"))
+    })
     @PutMapping("/changeState/{id}")
     public void changeState(@PathVariable Long id) {
         unitOfMeasureServicePort.changeState(id);
     }
 
-    /**
-     * Método para eliminar una unidad de medida por su identificador.
-     * @param id Identificador de la unidad de medida a eliminar.
-     */
+    @Operation(summary = "Eliminar una unidad de medida por su identificador", description = "Este endpoint permite eliminar una unidad de medida mediante su identificador.", responses = {
+            @ApiResponse(responseCode = "200", description = "Unidad de medida eliminada con éxito", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Unidad de medida no encontrada", content = @Content(mediaType = "application/json"))
+    })
     @DeleteMapping("/delete/{id}")
     public void deleteById(@PathVariable Long id) {
         unitOfMeasureServicePort.deleteById(id);
     }
 
-    /**
-     * Método para eliminar todas las unidades de medida.
-     */
+    @Operation(summary = "Eliminar todas las unidades de medida", description = "Este endpoint permite eliminar todas las unidades de medida del sistema.", responses = {
+            @ApiResponse(responseCode = "200", description = "Unidades de medida eliminadas con éxito", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Error interno al eliminar las unidades de medida", content = @Content(mediaType = "application/json"))
+    })
     @DeleteMapping("/deleteAll")
     public void deleteAll() {
         unitOfMeasureServicePort.deleteAll();

a2179decaaa423a47d436e92baf117d950f10cb4
JulianRuano <julianrmajin@gmail.com>
2025-07-05 13:48:09 -0500

    Refactor: update dependencies and configuration for PostgreSQL, add support for Eureka

--- src/main/java/com/products_management/ProductsManagementApplication.java
+++ src/main/java/com/products_management/ProductsManagementApplication.java
@@ -1,15 +1,15 @@
 package com.products_management;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Clase principal de la aplicación de gestión de productos.
  * Utiliza Spring Boot para inicializar la aplicación.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductsManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/products_management/infraestructure/input/rest/ProductTypeRestController.java
+++ src/main/java/com/products_management/infraestructure/input/rest/ProductTypeRestController.java
@@ -9,13 +9,20 @@ import io.swagger.v3.oas.annotations.Operation;
 
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import com.products_management.application.ports.input.IProductTypeServicePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;

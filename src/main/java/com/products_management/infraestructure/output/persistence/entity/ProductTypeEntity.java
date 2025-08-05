59dc341da3ee6dfa065ee7e92710f4c96a9686de
santiagonieto09 <santiago.nieto09@gmail.com>
2025-08-05 10:29:33 -0500

    refactor: Eliminacion de etiquetas de swagger

--- src/main/java/com/products_management/infraestructure/output/persistence/entity/ProductTypeEntity.java
+++ src/main/java/com/products_management/infraestructure/output/persistence/entity/ProductTypeEntity.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name = "product_type")
@@ -22,14 +21,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @NoArgsConstructor
 public class ProductTypeEntity {
 
-    @Schema(description = "Identificador único del tipo de producto")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Schema(description = "Nombre del tipo de producto")
     private String name;    
-    @Schema(description = "Descripción del tipo de producto")
     private String description;
-    @Schema(description = "Identificador de la empresa a la que pertenece el tipo de producto")
     private String enterpriseId;
 }

90194715219d1159ab73f343695f53c873c33e91
JulianRM <julianrmajin@gmail.com>
2025-09-22 18:27:29 -0500

    refactor: reorganize RabbitMQ configuration

---
+++ src/main/java/com/products_management/infraestructure/config/rabbitConfig/RabbitProductConfig.java
@@ -0,0 +1,50 @@
+package com.products_management.infraestructure.config.rabbitConfig;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+
+@Configuration
+@Slf4j
+@Profile("!test")
+public class RabbitProductConfig {
+    public static final String PRODUCT_EXCHANGE = "product.exchange";
+    public static final String PRODUCT_KARDEX_QUEUE = "product.kardex.queue";
+    public static final String PRODUCT_STOCK_QUEUE = "product.stock.queue";
+
+
+    @Bean
+    Queue productKardexQueue() {
+        return QueueBuilder.durable(PRODUCT_KARDEX_QUEUE).build();
+    }
+
+    @Bean
+    Queue productStockQueue() {
+        return QueueBuilder.durable(PRODUCT_STOCK_QUEUE).build();
+    }
+
+    @Bean
+    FanoutExchange productExchange() {
+        return new FanoutExchange(PRODUCT_EXCHANGE, true, false);
+    }
+
+    @Bean
+    Binding productKardexQueueBinding() {
+        return BindingBuilder.bind(productKardexQueue()).to(productExchange());
+    }
+
+    @Bean
+    Binding productStockQueueBinding() {
+        return BindingBuilder.bind(productStockQueue()).to(productExchange());
+    }
+
+
+}
